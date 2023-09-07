33e55e882b75f532b984c4d81ae7d19adaef1269
“mjcherono” <cherono.mercyjoy@gmail.com>
2023-09-07 18:40:02 +0300

    static files

--- crm1/settings.py
+++ crm1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts"
+    "accounts",
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/images/'
+
+STATIC_FILES_DIR = [
+    os.path.join(BASE_DIR, 'static' )
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
